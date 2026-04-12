e9fe7019d4c7ef19c837f48757808013380eca10
jangap411 <angapj@gmail.com>
2026-04-12 23:46:54 +1000

    Added capacitor to the project.

---
+++ android/app/src/main/java/com/gostret/rides/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gostret.rides;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+export const SOCKET_URL = API_BASE_URL.replace('/api', '');
 
 export const authService = {
   login: async (email, password) => {
